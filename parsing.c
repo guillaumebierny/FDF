8cb0f8a5ec8ee7baa9081210e96b0fa5b7737a74
Guillaume Bierny <gbierny@fu-r5-p5.s19.be>
2022-07-13 20:49:26 +0200

    mon fdf version final

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: gbierny <gbierny@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 00:11:44 by gbierny           #+#    #+#             */
-/*   Updated: 2022/06/14 03:43:57 by gbierny          ###   ########.fr       */
+/*   Updated: 2022/07/13 20:44:13 by gbierny          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,8 @@ void	read_file(char *argv, t_index *ind, char **whole_file)
 	char	*line;
 
 	fd = open(argv, O_RDONLY);
+	if (fd<0)
+		error_message("fichier invalide");
 	line = get_next_line(fd);
 	while (line)
 	{
@@ -84,11 +86,13 @@ void	get_point(char *argv, t_image *image, t_coor ***point)
 	read_file(argv, &ind[0], &whole_file);
 	*point = (t_coor **)malloc((ind[0].y + 1) * sizeof(t_coor *));
 	if (!*point)
-		exit(EXIT_FAILURE);
+		error_message("probleme de malloc");
 	printf("%s\n", whole_file);
 	all_split[0] = ft_split(whole_file, '\n');
 	loop_to_set_point(ind, all_split, point, image);
 	(*point)[ind[0].y] = malloc(sizeof(t_coor) * 1);
+	if (!(*point)[ind[0].y])
+		error_message("probleme de malloc");
 	(*point)[ind[0].y][0].exist = 0;
 	free_split(all_split[0]);
 }
@@ -107,17 +111,19 @@ void	dup_tab_point(t_coor **source, t_coor ***dest)
 		x++;
 	(*dest) = (t_coor **)malloc(sizeof(t_coor *) * (y + 1));
 	if (!*dest)
-		exit(EXIT_FAILURE);
+		error_message("probleme de malloc");
 	horizontal = 0;
 	while (horizontal < y)
 	{
 		(*dest)[horizontal] = (t_coor *)malloc(sizeof(t_coor)
 				* (x + 1));
 		if (!(*dest)[horizontal])
-			exit(EXIT_FAILURE);
+			error_message("probleme de malloc");
 		set_same_value(dest, source, x, horizontal);
 		horizontal++;
 	}
 	(*dest)[y] = malloc(sizeof(t_coor) * 1);
+	if (!(*dest)[y])
+		error_message("probleme de malloc");
 	(*dest)[y][0].exist = 0;
 }
